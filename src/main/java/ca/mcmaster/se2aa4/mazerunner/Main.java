7c8bc0cfd238e0ae9851b3ae8b0395f16a9d02b1
Simar Dhanda <simardhanda@MacBook-Air-3.local>
2025-01-23 02:52:59 -0500

    Finalized Main.java file

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,20 +1,47 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.*;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+
+        logger.info("** Starting Maze Runner");
+
+        //set up the CLI options
+        Options options = new Options();
+        options.addOption(Option.builder("i")
+                .longOpt("input")
+                .desc("Path to the input maze file")
+                .hasArg()
+                .argName("FILE")
+                .required()
+                .build());
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            logger.error("Failed to parse command-line arguments: " + e.getMessage());
+            formatter.printHelp("MazeRunner", options);
+            return;
+        }
+
+        //retrieve the input file path from the -i flag
+        String inputFilePath = cmd.getOptionValue("i");
+
+        try {
+            logger.info("**** Reading the maze from file " + inputFilePath);
+            BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
@@ -26,11 +53,13 @@ public class Main {
                 }
                 System.out.print(System.lineSeparator());
             }
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            reader.close();
+        } catch (Exception e) {
+            logger.error("/!\\\\ An error has occurred while reading the maze /!\\\\", e);
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+
+        logger.info("**** Computing path");
+        logger.info("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
     }
 }
